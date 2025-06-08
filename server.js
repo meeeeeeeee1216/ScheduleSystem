1c5acc65f1b7a853e0635c5421fb368bcfa62a41
meeeeeeeee1216 <211926725+meeeeeeeee1216@users.noreply.github.com>
2025-06-08 16:11:21 +0900

    add server

---
+++ server.js
@@ -0,0 +1,17 @@
+//サーバー構築
+// https://qiita.com/elu_jaune/items/eb354558d0dc39add152
+//ページ遷移
+//https://qiita.com/watsony/items/a6790d932d2f59589965
+
+
+
+const http = require("http");
+const server = http.createServer((req,res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    res.write('<h1>Hello World</h1>');
+    res.end();
+});
+
+const port = 8080;
+server.listen(port);
+console.log("")
