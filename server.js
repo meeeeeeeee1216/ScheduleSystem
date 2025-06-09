20c64fcde47bead90bcb825fcba8a780a0d088f0
meeeeeeeee1216 <211926725+meeeeeeeee1216@users.noreply.github.com>
2025-06-09 09:14:09 +0900

    add express

--- server.js
+++ server.js
@@ -2,10 +2,17 @@
 // https://qiita.com/elu_jaune/items/eb354558d0dc39add152
 //ページ遷移
 //https://qiita.com/watsony/items/a6790d932d2f59589965
+//Express
+//https://qiita.com/nkjm/items/723990c518acfee6e473
 
 
-
+//ロードモジュール
 const http = require("http");
+const express = require("express");
+
+
+const app = express();
+
 const server = http.createServer((req,res) => {
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
     res.write('<h1>Hello World</h1>');
