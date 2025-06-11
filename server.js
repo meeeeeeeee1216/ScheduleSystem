1a2c1b29c13e537855608102378f075ba3ba06d1
meeeeeeeee1216 <211926725+meeeeeeeee1216@users.noreply.github.com>
2025-06-11 12:29:06 +0900

    make server path

--- server.js
+++ server.js
@@ -4,21 +4,76 @@
 //https://qiita.com/watsony/items/a6790d932d2f59589965
 //Express
 //https://qiita.com/nkjm/items/723990c518acfee6e473
+//Express でHTMLを開く
+//https://www.i-ryo.com/entry/2020/04/16/215205
+//status code
+//https://qiita.com/syumiwohossu/items/ead2726731b9016edc87
+//getについて
+//https://qiita.com/syumiwohossu/items/f9ee317f31adc3ad387b
+
+//mysql
+//https://www.sejuku.net/blog/74849
 
 
-//ロードモジュール
+
+
+
+//モジュール
 const http = require("http");
 const express = require("express");
-
+const mysql = require("mysql");
 
 const app = express();
 
-const server = http.createServer((req,res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-    res.write('<h1>Hello World</h1>');
-    res.end();
+//DB接続
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: '*********',
+    database: "SSS"
+});
+con.connect((err) => {
+    if (err) throw err
+    console.log("Connected");
+})
+
+
+//リンク作成
+//ホーム画面
+app.get("/",(req,res) => {
+    res.sendFile(__dirname + "/html/home.html");
 });
 
+//ログイン画面
+app.get("/sign-in",(req,res) => {
+    res.sendFile(__dirname + "/html/signin.html");
+});
+//管理画面ホーム
+app.get("/administrator",(req,res) => {
+    res.sendFile(__dirname + "/html/administrator_page.html");
+});
+
+
+//ショー一覧画面
+app.get("/show",(req,res) => {
+    res.sendFile(__dirname + "/html/show_list.html");
+});
+
+//演者一覧画面
+app.get("/entertainer",(req,res) => {
+    res.sendFile(__dirname + "/html/entertainer_list.html");
+});
+
+
+//ポートを開く
 const port = 8080;
-server.listen(port);
-console.log("")
+app.listen(port);
+
+
+
+
+
+//ページ遷移
+function go_signin(){  
+
+}
