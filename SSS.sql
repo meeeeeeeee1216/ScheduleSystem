2053032f52b5360d77ecd7d9d4cabba06eb59c7c
meeeeeeeee1216 <211926725+meeeeeeeee1216@users.noreply.github.com>
2025-07-16 15:34:56 +0900

    報告保管テーブル作成

--- SSS.sql
+++ SSS.sql
@@ -66,25 +66,23 @@ FOREIGN KEY (entertainer_id) references entertainer(entertainer_id),
 FOREIGN KEY (show_id) references entertainment_show(show_id));
 
 -- 
+-- shift -> {'A役':'〇さん', ...} idにしない
+CREATE TABLE SSS.report(
+    report_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    shift JSON NOT NULL,
+    time_and_day DATETIME NOT NULL,
+    show_id INT
+);
+
 CREATE TABLE SSS.notice(
     notice_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     show_id INT not null,
     type_of_message varchar(30) not null,
     content text,
     report_id INT,
-    FOREIGN KEY (show_id) references entertainment_show(show_id)
+    FOREIGN KEY (show_id) references entertainment_show(show_id),
     FOREIGN KEY (report_id) references report(report_id)
 );
-
--- コードだけ書いてまだデータに入れてないよ～
--- shift -> {'A役':'〇さん', ...} idにしない
-CRATE TABLE SSS.report(
-    report_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    shift JSON NOT NULL
-    time_and_day DATETIME NOT NULL,
-    show_id INT
-);
-
 
 
 
